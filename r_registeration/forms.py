57a9d5df9d2b3cf787ec568f2c91e3f396e8f27f
root <hepaper3@gmail.com>
2019-05-14 03:31:26 +0000

    thrid commit

--- r_registeration/forms.py
+++ r_registeration/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from .models import Blog
 
+# 폼 수정은 이거 보고 다시 하기 : https://docs.djangoproject.com/en/2.2/topics/forms/
+
 class BlogForm(forms.ModelForm):
     class Meta:
         model = Blog
